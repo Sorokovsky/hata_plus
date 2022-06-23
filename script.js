b189cb63c81cd867536992991051e9b4c6a15a46
Sorokovsky <Sorokovskys@ukr.net>
2022-06-23 10:26:19 +0300

    popub btn fix

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const popub = document.querySelector('#popub');
 const call = document.querySelector('.header__btn');
 if (call){
-    call.addEventListener('click', () => {
+    call.addEventListener('click', (e) => {
+        e.preventDefault();
         if (popub && !popub.classList.contains('_active') ) {
             popub.classList.add('_active');
             document.body.classList.add('_lock');            
@@ -10,6 +11,7 @@ if (call){
 }
 document.body.addEventListener('click', (e) => {
     const { target } = e;
+    e.preventDefault();
     if ((target.closest('.popub') && !target.closest('.popub__content') || target.classList.contains('popub__close'))) {
         popub.classList.remove('_active');
         document.body.classList.remove('_lock');
