2847d155c1888c8b3722892e3d25203f836d2bcc
Sorokovsky <Sorokovskys@ukr.net>
2022-06-24 11:17:37 +0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const popub = document.querySelector('#popub');
 const call = document.querySelector('.header__btn');
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header__menu');
 if (call){
     call.addEventListener('click', (e) => {
         e.preventDefault();
         if (popub && !popub.classList.contains('_active') ) {
             popub.classList.add('_active');
             document.body.classList.add('_lock');            
+        }
+    });
+}
+if (burger){
+    burger.addEventListener('click', (e) => {
+        if(menu){
+            const { target } = e;
+            if(menu.classList.contains('_active')){
+                menu.classList.remove('_active');
+                burger.classList.remove('_active');
+                burger.children[0].classList.remove('_active');
+                document.body.classList.remove('_lock');
+            }else{
+                menu.classList.add('_active');
+                burger.classList.add('_active');
+                burger.children[0].classList.add('_active');
+                document.body.classList.add('_lock');
+            }
         }
     });
 }
